1c8e66bad828584c37b598e4b4630c7dd356253c
Craig Jones <Craig.Jones@utas.edu.au>
2015-04-23 11:18:23 +1000

    Use jndi reference in production

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -6,12 +6,18 @@ dataSource {
 def env = System.getenv()
 
 environments {
-	development {
-	    dataSource {
-	        driverClassName = "org.postgresql.Driver"
-	        url = env['DATA_SOURCE_URL']
-	        username = env['DATA_SOURCE_USERNAME']
-	        password = env['DATA_SOURCE_PASSWORD']
-	    }
-	}
+    development {
+        dataSource {
+            driverClassName = "org.postgresql.Driver"
+            url = env['DATA_SOURCE_URL']
+            username = env['DATA_SOURCE_USERNAME']
+            password = env['DATA_SOURCE_PASSWORD']
+        }
+    }
+
+    production {
+        dataSource {
+            jndiName = "java:comp/env/jdbc/harvest_read"
+        }
+    }
 }
