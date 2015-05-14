be2789a1d15dfdd94e35819a930d506b5e541283
David Nahodil <dnahodil@utas.edu.au>
2015-05-14 13:25:59 +1000

    Time each wget command

--- test_jodaac.sh
+++ test_jodaac.sh
@@ -6,7 +6,7 @@ NUM_CONCURRENT_JOBS=${2:-$NUM_JOBS}
 
 JODAAC_HOST="http://localhost:8080/netcdf-subset"
 JODAAC_URL="$JODAAC_HOST/subset?typeName=anmn_ts&CQL_FILTER=INTERSECTS(geom,POLYGON((113.33 -33.09,113.33 -30.98,117.11 -30.98,117.11 -33.09,113.33 -33.09))) AND TIME >= %272015-01-13T23:00:00Z%27 AND TIME <= %272015-04-14T00:00:00Z%27"
-JODAAC_COMMAND="wget '$JODAAC_URL' -O 'download{#}.zip'"
+JODAAC_COMMAND="time wget '$JODAAC_URL' -O 'download{#}.zip'"
 
 echo "Starting $NUM_JOBS jobs..."
 seq "$NUM_JOBS" | parallel -j"$NUM_CONCURRENT_JOBS" $JODAAC_COMMAND
